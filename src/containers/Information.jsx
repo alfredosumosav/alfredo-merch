bd9b31ee72315da5a5a436f278bdcc5052543bf1
alfredosumosav <asumosa1993@gmail.com>
2021-07-03 13:58:08 -0500

    add checkout workflow routes

---
+++ src/containers/Information.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Information = () => <h1>Information</h1>;
+
+export default Information;
---
+++ src/containers/Success.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Success = () => <h1>Success</h1>;
+
+export default Success;
