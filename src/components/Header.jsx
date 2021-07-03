28aca1e619678e43d4e20e6f071b5ffd6bab250b
alfredosumosav <asumosa1993@gmail.com>
2021-07-03 14:24:52 -0500

    implement initial header and footer components

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/components/Header.css';
+
+const Header = () => (
+    <div className="Header">
+      <h1 className="Header-title">Alfredo Merch</h1>
+      <div className="Header-checkout">
+        Checkout
+      </div>
+    </div>
+  )
+
+export default Header
