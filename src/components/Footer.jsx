98bd806d754bd68064b006ee3f7c8bbbaef6d6d1
alfredosumosav <asumosa1993@gmail.com>
2021-07-03 14:28:57 -0500

    format code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 import '../styles/components/Footer.css';
 
 const Footer = () => (
-    <div className="Footer">
-      <p className="Footer-title">Alfredo Merch</p>
-      <p className="copy">All Lefts Reserved</p>
-    </div>
-  )
+  <div className="Footer">
+    <p className="Footer-title">Alfredo Merch</p>
+    <p className="copy">All Lefts Reserved</p>
+  </div>
+);
 
-export default Footer
+export default Footer;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -12,16 +12,16 @@ import Layout from '../components/Layout';
 const App = () => (
   <div>
     <BrowserRouter>
-    <Layout>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/checkout" component={Checkout} />
-        <Route exact path="/checkout/information" component={Information} />
-        <Route exact path="/checkout/payment" component={Payment} />
-        <Route exact path="/checkout/success" component={Success} />
-        <Route component={NotFound} />
-      </Switch>
-    </Layout>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/checkout" component={Checkout} />
+          <Route exact path="/checkout/information" component={Information} />
+          <Route exact path="/checkout/payment" component={Payment} />
+          <Route exact path="/checkout/success" component={Success} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
     </BrowserRouter>
   </div>
 );
