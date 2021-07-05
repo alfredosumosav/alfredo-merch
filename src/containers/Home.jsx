395ef8c37f63cae5fbf02b361e947d04e3cc9190
alfredosumosav <asumosa1993@gmail.com>
2021-07-05 18:23:29 -0500

    style products list

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import Products from "../components/Products";
+import initialState from "../initialState";
 
-const Home = () => <h1>Home</h1>;
+const Home = () => <Products products={initialState.products} />;
 
 export default Home;
