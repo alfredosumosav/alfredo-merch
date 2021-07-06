0d2c073302c37291378342e7052c5839ffc84f2b
alfredosumosav <asumosa1993@gmail.com>
2021-07-06 18:15:41 -0500

    add checkout styles

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import "../styles/components/Checkout.css";
 
-const Checkout = () => <h1>Checkout</h1>;
+const Checkout = () =>
+<div className="Checkout">
+  <div className="Checkout-content">
+    <h3>Lista de Pedido:</h3>
+    <div className="Checkout-item">
+      <div className="Checkout-element">
+        <h4>ITEM name</h4>
+        <span>$10</span>
+      </div>
+      <button type="button">Eliminar</button>
+    </div>
+  </div>
+  <div className="Checkout-sidebar">
+    <h3>Precio Total: $10</h3>
+    <button type="button">Continuar Pedido</button>
+  </div>
+</div>;
 
 export default Checkout;
