50e33eb51f32e0a6d8eb7a5151f13be21b03e07a
alfredosumosav <asumosa1993@gmail.com>
2021-07-06 22:59:16 -0500

    implement custom hook for initial state logic

---
+++ src/hooks/useInitialState.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import initialState from '../initialState';
+
+const useInitialState = () => {
+  const [state, setState] = useState(initialState);
+
+  const addToCart = (payload) => {
+    setState({
+      ...state,
+      cart: [...state.cart, payload],
+    });
+  };
+
+  const removeFromCart = (payload) => {
+    setState({
+      ...state,
+      cart: state.cart.filter((item) => item.id !== payload.id),
+    });
+  };
+
+  return {
+    addToCart,
+    removeFromCart,
+    state,
+  };
+};
+
+export default useInitialState;
