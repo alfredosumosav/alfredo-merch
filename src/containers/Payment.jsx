86fe63659a10e69c88096434ca2e22c5f261e9bf
alfredosumosav <asumosa1993@gmail.com>
2021-07-09 21:26:19 -0500

    integrate paypal api into payment button

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,13 +1,71 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { PayPalButton } from 'react-paypal-button';
+import AppContext from '../context/AppContext';
+import handleSumTotal from '../utils/handleSumTotal';
 import '../styles/components/Payment.css';
 
-const Payment = () => (
-  <div className="Payment">
-    <div className="Payment-content">
-      <h3>Resumen del pedido:</h3>
-      <div className="Payment-button">Boton de pago con Paypal</div>
+const Payment = () => {
+  const { state, addNewOrder } = useContext(AppContext);
+  const { cart, buyer } = state;
+  const history = useHistory();
+
+  const paypalOptions = {
+    clientId:
+      'ASEah8GVeVUdymX1v0J-qhZd34AHnAxVO9u9Dh4KZSPZGy3JIbOJN5_d9WDp55fsf8K1NoU5loa1xpdX',
+    intent: 'capture',
+    currency: 'USD',
+  };
+
+  const buttonStyles = {
+    layout: 'vertical',
+    shape: 'rect',
+  };
+
+  const amount = handleSumTotal(cart);
+
+  const handlePaymentSuccess = (data) => {
+    console.log(data);
+    if (data.status === 'COMPLETED') {
+      const newOrder = {
+        buyer,
+        products: cart,
+        payment: data,
+      };
+      addNewOrder(newOrder);
+      history.push('/checkout/success');
+    }
+  };
+
+  return (
+    <div className="Payment">
+      <div className="Payment-content">
+        <h3>Resumen del pedido:</h3>
+        {cart.map((item) => (
+          <div className="Payment-item" key={item.title}>
+            <div className="Payment-element">
+              <h4>{item.title}</h4>
+              <span>${item.price}</span>
+            </div>
+          </div>
+        ))}
+        <div className="Payment-total">
+          <span>Total: {amount}</span>
+        </div>
+        <div className="Payment-button">
+          <PayPalButton
+            paypalOptions={paypalOptions}
+            buttonStyles={buttonStyles}
+            amount={amount}
+            onPaymentStart={() => console.log('Start')}
+            onPaymentSuccess={handlePaymentSuccess}
+            onPaymentError={(error) => console.log(error)}
+            onPaymentCancel={(data) => console.log(data)}
+          />
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Payment;
