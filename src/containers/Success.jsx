184ac852f5d7ea3d46919e2240b8f50eebac7dbc
alfredosumosav <asumosa1993@gmail.com>
2021-07-10 12:50:32 -0500

    implement custom hook for geolocation logic

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,19 +1,25 @@
 import React, { useContext } from 'react';
 import AppContext from '../context/AppContext';
 import Map from '../components/Map';
+import useGoogleAddress from '../hooks/useGoogleAddress';
 import '../styles/components/Success.css';
 
 const Success = () => {
   const { state } = useContext(AppContext);
   const { buyer } = state;
+  const location = useGoogleAddress(
+    buyer.length ? buyer[0].address : 'bocagrande'
+  );
 
   return (
     <div className="Success">
       <div className="Success-content">
-        <h2>{`${buyer.name}, Gracias por tu compra`}</h2>
+        <h2>{`${
+          buyer.length ? buyer[0].name : 'testUser'
+        }, Gracias por tu compra`}</h2>
         <span>Tu pedido llegara en 3 dias a tu direccion:</span>
         <div className="Success-map">
-          <Map />
+          <Map data={location} />
         </div>
       </div>
     </div>
---
+++ src/hooks/useGoogleAddress.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useGoogleAddress = (address) => {
+  const [map, setMap] = useState({});
+  const API = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
+
+  useEffect(async () => {
+    const response = await axios(API);
+    setMap(response.data.results[0].geometry.location);
+  }, []);
+
+  return map;
+};
+
+export default useGoogleAddress;
