a3e5c84cc24a44417a9c2283166389269ea43a13
alfredosumosav <asumosa1993@gmail.com>
2021-08-06 11:49:49 -0700

    [add] product tests

---
+++ src/__mocks__/ProductMock.js
@@ -0,0 +1,19 @@
+const ProductMock = {
+  id: 1,
+  title: 'Hoodie Original',
+  price: 10,
+  description: 'Description 1',
+  uid: 'products',
+  published_at: '2021-07-21T19:53:07.343Z',
+  created_at: '2021-07-21T19:53:05.254Z',
+  updated_at: '2021-07-21T19:56:27.161Z',
+  image: [
+    {
+      id: 1,
+      name: 'hoodie0.jpg',
+      url: 'https://alfredosumosa.s3.us-west-1.amazonaws.com/alfredo-merch-assets/hoodie0.jpg',
+    },
+  ],
+};
+
+export default ProductMock;
---
+++ src/__test__/components/Product.test.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import ProviderMock from '../../__mocks__/ProviderMock';
+import ProductMock from '../../__mocks__/ProductMock';
+import Product from '../../components/Product';
+
+describe('<Product />', () => {
+  test('Product component renders', () => {
+    const product = shallow(
+      <ProviderMock>
+        <Product />
+      </ProviderMock>
+    );
+    expect(product.length).toEqual(1);
+  });
+  test('Buy button test', () => {
+    const handleAddToCart = jest.fn();
+    const wrapper = mount(
+      <ProviderMock>
+        <Product product={ProductMock} handleAddToCart={handleAddToCart} />
+      </ProviderMock>
+    );
+    wrapper.find('button').simulate('click');
+    expect(handleAddToCart).toHaveBeenCalledTimes(1);
+  });
+});
