1869bca3540383d519b739f01b604decd48a89ba
alfredosumosav <asumosa1993@gmail.com>
2021-08-06 11:31:18 -0700

    [add] Header tests

---
+++ src/__test__/components/Header.test.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import ProviderMock from '../../__mocks__/ProviderMock';
+import Header from '../../components/Header';
+
+describe('<Header />', () => {
+  test('Header component renders', () => {
+    const header = shallow(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>
+    );
+    expect(header.length).toEqual(1);
+  });
+
+  test('Header title renders', () => {
+    const header = mount(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>
+    );
+    expect(header.find('.Header-title').text()).toEqual('Alfredo Merch');
+  });
+});
